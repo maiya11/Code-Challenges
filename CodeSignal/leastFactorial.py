9a813c4f45c847bb6e141f238ca56c55f7e3b8c4
Maiya <112497137+maiya11@users.noreply.github.com>
2023-04-24 14:51:13 -0700

    Create leastFactorial.py

---
+++ CodeSignal/leastFactorial.py
@@ -0,0 +1,22 @@
+'''
+Given an integer n, find the minimal k such that
+
+k = m! (where m! = 1 * 2 * ... * m) for some integer m;
+k >= n.
+In other words, find the smallest factorial which is not less than n.
+
+Example
+
+For n = 17, the output should be
+solution(n) = 24.
+
+17 < 24 = 4! = 1 * 2 * 3 * 4, while 3! = 1 * 2 * 3 = 6 < 17).
+'''
+
+def solution(n):
+    k, m = 1, 1
+    while k < n:
+        k = m * k
+        m += 1
+        
+    return k
